03430594c77d53de513961fbd8769c6ba0ab9866
justin <justin.ch.goh@gmail.com>
2018-07-13 11:50:32 +0800

    tutorial 6

--- 6-remoteprocedurecall/rpc_client.js
+++ 6-remoteprocedurecall/rpc_client.js
@@ -18,12 +18,33 @@ const generateUuid = function generateUuid() {
 
 amqp.connect('amqp://localhost', function(err, conn) {
   conn.createChannel(function(err, ch) {
-    // anonymous channel?
+
+    // tutorial says: creates an anonymous exclusive
+    // callback queue
+    
+    // according to channel.assertQueue docs, 
+    // an empty string in the queue parameter 
+    // makes the server create a random name
+    // {exclusive: true} scopes the queue to the connection
+    // doing rabbitmqctl list_queues doesn't show this queue
+    
+    // assertqueue Creates a queue if it doesn't exist
+    // we're creating our own queue for us to "sub" to
     ch.assertQueue('', {exclusive: true}, function(err, q) {
+
+      // instead of constantly opening and closing an anonymous exclusive callback queue,
+      // we leave the callback queue open
+      // and use correlationIds for separate requests/messages/replies
+
       var corr = generateUuid();
+      
+      // error checking for float, but no non negative validation
       var num = parseInt(args[0]);
 
       console.log(` [x] Requesting fib(${num})`);
+
+      // q.queue is the randomly named queue
+      // console.log(q.queue);
 
       ch.consume(q.queue, function(msg) {
         if (msg.properties.correlationId == corr) {
@@ -35,6 +56,8 @@ amqp.connect('amqp://localhost', function(err, conn) {
         }
       }, {noAck: true});
 
+      // we're creating a message that expects a reply
+      // specifically to the queue we created in assertqueue
       ch.sendToQueue('rpc_queue',
         new Buffer(num.toString()),
         { correlationId: corr, replyTo: q.queue}
